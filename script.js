24e345b0c008b10931c5fec7a30c2b13991ac1b1
Mike <madetunji1376@gmail.com>
2022-06-21 22:44:36 +0100

    mobile-menu work done

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".NavMenu");
+const Close = document.querySelector(".Close");
+
+hamburger.onclick = () => {
+  //call-back function
+  navMenu.classList.add("mike");
+};
+
+Close.onclick = () => {
+  //call-back function
+  navMenu.classList.remove("mike");
+};
