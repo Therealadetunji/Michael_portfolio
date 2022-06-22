573eeb12f45788c9f403e4ee589b3ba52de4202b
milen-ale <milenalebel2@gmail.com>
2022-06-22 01:15:11 -0700

    added code for style and javascript to make the close button responsive

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".NavMenu");
-const Close = document.querySelector(".Close");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.NavMenu');
+const Close = document.querySelector('.Close');
 
 hamburger.onclick = () => {
   //call-back function
-  navMenu.classList.add("mike");
+  navMenu.classList.add('mike');
+};
+Close.onclick = () => {
+  //call-back function
+  navMenu.classList.remove('mike');
 };
