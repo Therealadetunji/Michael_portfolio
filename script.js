763b8f4716cab39655a4a907c8db9b09c86ff84e
Mike <madetunji1376@gmail.com>
2022-06-22 11:18:05 +0100

    making correction from reviewer

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".NavMenu");
 const Close = document.querySelector(".Close");
+const Navitem = document.querySelector(".nav-link1");
+const Navitem2 = document.querySelector(".nav-link2");
+const Navitem3 = document.querySelector(".nav-link3");
 
 hamburger.onclick = () => {
   //call-back function
@@ -8,5 +11,17 @@ hamburger.onclick = () => {
 };
 Close.onclick = () => {
   //call-back function
+  navMenu.classList.remove("mike");
+};
+
+Navitem.onclick = () => {
+  navMenu.classList.remove("mike");
+};
+
+Navitem2.onclick = () => {
+  navMenu.classList.remove("mike");
+};
+
+Navitem3.onclick = () => {
   navMenu.classList.remove("mike");
 };
