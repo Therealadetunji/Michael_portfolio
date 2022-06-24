eb70e2f49b8359655499abb3bca1f4f5bd5353dd
milen-ale <milenalebel2@gmail.com>
2022-06-24 03:18:06 -0700

    fix background color of the erorr message

--- form-val.js
+++ form-val.js
@@ -1,8 +1,6 @@
-// const email = document.getElementById('email');
 const submit = document.querySelector('.get-touch');
 submit.addEventListener('click', validate);
 function validate(e) {
-  //   e.preventDefault();
   const email = document.getElementById('email');
   if (email.value === email.value.toLowerCase()) {
     alert('done');
@@ -12,28 +10,3 @@ function validate(e) {
     err.innerHTML = 'Email cannot be uppercase';
   }
 }
-
-// (email.value !== email.value.toLowerCase()) {
-//     const err = document.querySelector('.err');
-//     // err.classList.toggle('show');
-//     e.preventDefault();
-//     err.innerHTML = 'error';
-//   } else if (email.value === email.value.toLowerCase()) {
-//     // const suc = document.querySelector('.suc');
-//     // suc.classList.toggle('visible');
-//     alert('Done');
-//   }
-
-// const submit = document.querySelector('.get-touch');
-// submit.addEventListener('click', validate);
-// function validate(e) {
-//   //   e.preventDefault();
-//   const email = document.getElementById('email');
-//   if (email.value === email.value.toLowerCase()) {
-//     alert('done');
-//   } else {
-//     const err = document.querySelector('.err');
-//     e.preventDefault();
-//     err.innerHTML = 'error';
-//   }
-// }
